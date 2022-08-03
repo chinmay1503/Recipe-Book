9295371c0384d0aa461a29146a6cd3cf86a55a38
Chinmay Tawde <ctawde@pdx.edu>
2022-08-03 02:28:55 -0500

    Added Speech Recognition

--- client/recipe-book-spa/src/Home.js
+++ client/recipe-book-spa/src/Home.js
@@ -4,6 +4,21 @@ import {
     HashRouter,
     
 } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ToastContainer, toast } from 'material-react-toastify';
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+let recognition;
+
+if (SpeechRecognition) {
+    recognition = new SpeechRecognition();
+} else {
+    toast.dark("Sorry the browser doesn't support Speech Recognition!", {
+        position:"top-right",
+        autoClose: 3000,
+        closeOnClick: true
+    });
+}
 
 class Home extends Component {
     constructor(props) {
@@ -11,6 +26,7 @@ class Home extends Component {
         this.setTerm = this.setTerm.bind(this);
         this.onKeyDown = this.onKeyDown.bind(this);
         this.onClick = this.onClick.bind(this);
+        this.onMicrophoneClick = this.onMicrophoneClick.bind(this);
         this.state = {
           searchTerm: "",
           placeholder: "Search for a Recipe...",
@@ -59,19 +75,58 @@ class Home extends Component {
         }
     }
     
+    onMicrophoneClick(event) 
+    {
+        if (SpeechRecognition) {
+            const micBtn = event.currentTarget;
+            const micIcon = micBtn.querySelector("svg");
+
+            if (!micIcon.classList.contains("fa-beat-fade")) {
+                // Start Speech Recoginition
+                recognition.start();
+            } else {
+                // Stop Speech Recoginition
+                recognition.stop();
+            }
+
+
+            recognition.onstart = function() {
+                console.log("Speech Recognition active");
+                micIcon.classList.add("fa-beat-fade");
+            }
+
+            recognition.onend = function() {
+                console.log("Speech Recognition stopped");
+                micIcon.classList.remove("fa-beat-fade");
+            }
+
+            recognition.onerror = function() {
+                console.log("Something went wrong while listening! Speech Recognition stopped");
+            }
+
+            recognition.addEventListener('result', function(event) {
+                const transcript = event.results[0][0].transcript;
+                this.setState({ searchTerm: transcript });
+                const searchBox = document.getElementById("searchBox");
+                searchBox.value = transcript;
+              }.bind(this));
+        }
+    }
     
     render(){
         return(
             <HashRouter>
                 <div className = "contentContainer">
+                <ToastContainer />
                     <div className = "spatulaContainer">
                         <img className = "spatula" alt="SpatulaImg" src = "/images/spatulaImage.png"/>
                     </div>
                     <div className="mainContainer">
-                        <h1>Find a New Recipe</h1>
+                        <h1>Find a New Recipe </h1>
                         <div className = "containSearch">
                             <div className = "searchContainer">
-                                <input onKeyDown={this.onKeyDown} className = "searchBox shadow" type="text" onChange={this.setTerm} placeholder= {this.state.placeholder}/>
+                                <input id="searchBox" onKeyDown={this.onKeyDown} className = "searchBox shadow" type="text" onChange={this.setTerm} placeholder= {this.state.placeholder}/>
+                                <button className = "micButton" type="button" onClick = {this.onMicrophoneClick}><FontAwesomeIcon icon="fas fa-microphone" /></button>
                                 <button  className = "searchButton" onClick = {this.onClick}>
                                     <NavLink id = "submit" className = {this.state.navClass} to={`/search/${this.state.searchTerm}`}><img className = "searchIcon" alt="SearchIcon" src = "/images/searchIcon.png"/></NavLink> 
                                 </button>                
--- client/recipe-book-spa/src/index.js
+++ client/recipe-book-spa/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
-import "./css/styles.css"
-import "./css/homeStyles.css"
-import "./css/browseStyles.css"
-import "./css/searchStyles.css"
+import "./css/styles.css";
+import "./css/homeStyles.css";
+import "./css/browseStyles.css";
+import "./css/searchStyles.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, faTwitter, faFontAwesome)
  
 ReactDOM.render(
   <Main/>, 
